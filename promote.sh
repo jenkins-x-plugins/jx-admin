56c2b50b165bb0ebfba8e7e9280ba45e4a61c53d
James Strachan <james.strachan@gmail.com>
2020-07-02 06:24:19 +0100

    chore: fix promote

--- promote.sh
+++ promote.sh
@@ -2,12 +2,6 @@
 
 echo "promoting the new version ${VERSION} to downstream repositories"
 
-#jx step create pr go --name github.com/jenkins-x/jx-admin --version ${VERSION} --build "make build" --repo https://github.com/jenkins-x/jxl.git
+jx step create pr regex --regex 'AdminVersion = "(?P<version>.*)"' --version ${VERSION} --files pkg/plugins/version.go --repo https://github.com/jenkins-x/jx-cli.git
 
-#jx step create pr regex --regex "^(?m)\s+name: helmboot\s+version: \"(.*)\"$"  --version ${VERSION} --files alpha/plugins.yml --repo https://github.com/jenkins-x/jxl.git
-
-#jx step create pr regex --regex "^jxRemoteVersion:\s+\"(.*)\"$"  --version ${VERSION} --files charts/jx-app-cb-remote/values.yaml --repo https://github.com/cloudbees/jx-app-cb-remote.git
-
-#jx step create pr regex --regex "/cloudbees-jx-admin-plugin/plugin/(.*)/"  --version ${VERSION} --files README.md --repo https://github.com/cloudbees/jx-app-cb-remote.git
-
-#jx step create pr regex --regex "/cloudbees-jx-admin-plugin/plugin/(.*)/"  --version ${VERSION} --files remote-environments/README.md --repo https://github.com/cloudbees/jx-previews.git
+jx step create pr regex --regex 'version: (.*)' --version ${VERSION} --files docker/gcr.io/jenkinsxio-labs-private/jx-admin.yml --repo https://github.com/jenkins-x/jxr-versions.git
