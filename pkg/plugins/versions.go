7a619c4be05a3cc7852005593c95e6b30969e778
James Strachan <james.strachan@gmail.com>
2020-08-11 12:44:46 +0100

    fix: upgrade plugin versions

--- pkg/plugins/versions.go
+++ pkg/plugins/versions.go
@@ -2,10 +2,10 @@ package plugins
 
 const (
 	// HelmVersion the default version of helm to use
-	HelmVersion = "3.2.1"
+	HelmVersion = "3.2.4"
 
 	// HelmfileVersion the default version of helmfile to use
-	HelmfileVersion = "0.115.0"
+	HelmfileVersion = "0.125.2"
 
 	// HelmAnnotateVersion the default version of helmfile-annotate to use
 	HelmAnnotateVersion = "0.0.11"
