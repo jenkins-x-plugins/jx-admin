beb324089857ee2439b342cfce09c246d14e44e6
PratikforCoding <kotalpratik@gmail.com>
2022-11-27 16:15:56 +0530

    updated jenkins-x-release.yaml and upload-binaries.sh
    Signed-off-by: PratikforCoding <kotalpratik@gmail.com>

--- .github/workflows/jenkins-x/upload-binaries.sh
+++ .github/workflows/jenkins-x/upload-binaries.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-echo "HOME is $HOME"
-echo current git configuration
-
 # See https://github.com/actions/checkout/issues/766
 git config --global --add safe.directory "$GITHUB_WORKSPACE"
 git config --global --get user.name
@@ -13,20 +10,10 @@ echo "setting git user"
 git config --global user.name jenkins-x-bot-test
 git config --global user.email "jenkins-x@googlegroups.com"
 
-git add * || true
-git commit -a -m "chore: release $VERSION" --allow-empty
-git tag -fa v$VERSION -m "Release version $VERSION"
-git push origin v$VERSION
-
 export BRANCH=$(git rev-parse --abbrev-ref HEAD)
 export BUILDDATE=$(date)
 export REV=$(git rev-parse HEAD)
-export GOVERSION="1.17.9"
+export GOVERSION="$(go version | sed -e 's/^[^0-9.]*\([0-9.]*\).*/\1/')"
 export ROOTPACKAGE="github.com/$REPOSITORY"
-
-# Install syft in this script
-curl -sSfL https://raw.githubusercontent.com/anchore/syft/main/install.sh | \
-sh -s -- -b /usr/local/bin v0.54.0
-chmod +x /usr/local/bin/syft
 
 goreleaser release
